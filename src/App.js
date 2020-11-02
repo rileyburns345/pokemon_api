d5ef039e8ba5498f4d1b47973f17cdf355cadacd
Riley Burns <rileyburns345@gmail.com>
2020-11-02 15:00:14 -0700

    assigment complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+  const [pokemonList, setPokemonList] = useState([])
+
+  const getPokemon = (e) => {
+    e.preventDefault()
+    console.log('hello3')
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+          .then(response => {
+            return response.json();
+        }).then(response => {
+            console.log(response);
+            setPokemonList(response.results)
+        }).catch(err=>{
+            console.log(err);
+        });
+
+  }
+  
+  console.log('pokemon:', pokemonList)
+  
+
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getPokemon}>
+      <button type="submit">Fetch Pokemon</button>
+      </form>
+      {
+        pokemonList.map((pokemon) => 
+          <div>{pokemon.name}</div>
+        )
+      }
+
+      {
+        console.log('ymmm')
+      }
     </div>
   );
 }
